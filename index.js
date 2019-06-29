ef8aa68726633084de8abfc82cc494681e212d15
Sean Lavine <freewil@users.noreply.github.com>
2019-06-29 03:44:48 -0400

    add standard

--- index.js
+++ index.js
@@ -1,179 +1,178 @@
-const jwt = require('jsonwebtoken');
-const http2 = require('http2');
-const querystring = require('querystring');
+'use strict'
+
+const jwt = require('jsonwebtoken')
+const http2 = require('http2')
+const querystring = require('querystring')
 
 module.exports = class ApplePush {
+  /**
+   * Constructor method for the ApplePush class.
+   * Will set the `url` attribute to the sandbox or production
+   * urls for the APNS server depending upon the value
+   * of the NODE_ENV environment variable.
+   *
+   * @return {ApplePush} A new instance of type ApplePush
+   */
+  constructor () {
+    if (process.env.NODE_ENV === 'production') {
+      this.url = 'https://api.push.apple.com'
+    } else {
+      this.url = 'https://api.sandbox.push.apple.com'
+    }
 
-	/**
-	 * Constructor method for the ApplePush class.
-	 * Will set the `url` attribute to the sandbox or production
-	 * urls for the APNS server depending upon the value
-	 * of the NODE_ENV environment variable.
-	 * 
-	 * @return {ApplePush} A new instance of type ApplePush
-	 */
-	 constructor() {
-	 	if (process.env.NODE_ENV === "production") {
-	 		this.url = "https://api.push.apple.com";
-	 	} else {
-	 		this.url = "https://api.sandbox.push.apple.com";
-	 	}
+    /**
+      * A map of the various network errors that can be encountered
+      * while sending out the push to APNS.
+      *
+      * @type {Object}
+      */
+    this.ERRORS = {
+      400: 'Bad request',
+      403: 'There was an error with the certificate or with the provider’s authentication token.',
+      405: 'The request used an invalid :method value. Only POST requests are supported.',
+      410: 'The device token is no longer active for the topic.',
+      413: 'The notification payload was too large.',
+      429: 'The server received too many requests for the same device token.',
+      500: 'Internal server error.',
+      503: 'The server is shutting down and unavailable.'
+    }
+  }
 
-	 	/**
-	 	 * A map of the various network errors that can be encountered
-	 	 * while sending out the push to APNS.
-	 	 * 
-	 	 * @type {Object}
-	 	 */
-	 	 this.ERRORS = {
-	 	 	400: "Bad request",
-	 	 	403: "There was an error with the certificate or with the provider’s authentication token.",
-	 	 	405: "The request used an invalid :method value. Only POST requests are supported.",
-	 	 	410: "The device token is no longer active for the topic.",
-	 	 	413: "The notification payload was too large.",
-	 	 	429: "The server received too many requests for the same device token.",
-	 	 	500: "Internal server error.",
-	 	 	503: "The server is shutting down and unavailable."
-	 	 };
+  /**
+   * Sends a new push notification via the APNS service
+   * The method uses the APNS service in a stateless manner making use of a shortlived
+   * HTTP/2 session.
+   *
+   * @param  {Any} payload - Can be a `string` or `object`  to be posted
+   * @param  {String} jwt - json web token to sent for authentication
+   *
+   * @return {Promise} A promise that resolves if the request is successful or rejects
+   * with an error
+   */
+  push (payload, jwt, deviceToken, bundleId, options) {
+    return new Promise((resolve, reject) => {
+      if (!payload) {
+        reject(Error('Parameter `payload` is required'))
+        return
+      }
+      if (!jwt) {
+        reject(Error('Parameter `jwt` is required'))
+        return
+      }
+      if (!deviceToken) {
+        reject(Error('Parameter `deviceToken` is required'))
+        return
+      }
+      if (!bundleId) {
+        reject(Error('Parameter `bundleId` is required'))
+        return
+      }
+      const session = http2.connect(this.url)
+      const sessionErrorHandler = (error) => {
+        session.destroy()
+        reject(error)
+      }
 
-	 	}
+      session.on('error', sessionErrorHandler)
+      session.on('socketError', sessionErrorHandler)
+      session.on('goaway', sessionErrorHandler)
 
-	/**
-	 * Sends a new push notification via the APNS service
-	 * The method uses the APNS service in a stateless manner making use of a shortlived
-	 * HTTP/2 session.
-	 * 
-	 * @param  {Any} payload - Can be a `string` or `object`  to be posted  
-	 * @param  {String} jwt - json web token to sent for authentication
-	 * 
-	 * @return {Promise} A promise that resolves if the request is successful or rejects
-	 * with an error 
-	 */
-	 push (payload, jwt, deviceToken, bundleId, options) {
-	 	return new Promise((resolve, reject) => {
-	 		if (!payload) {
-	 			reject(Error('Parameter `payload` is required'));
-	 			return;
-	 		}
-	 		if (!jwt) {
-	 			reject(Error('Parameter `jwt` is required'));
-	 			return;	
-	 		}
-	 		if (!deviceToken) {
-	 			reject(Error('Parameter `deviceToken` is required'));
-	 			return;	
-	 		}
-	 		if (!bundleId) {
-	 			reject(Error('Parameter `bundleId` is required'));
-	 			return;	
-	 		}
-	 		const session = http2.connect(this.url);
-	 		const sessionErrorHandler = (error) => {
-	 			session.destroy();
-	 			reject(error);
-	 		}
+      let headers = {
+        ':path': `/3/device/${deviceToken}`,
+        ':method': 'POST',
+        'authorization': `bearer ${jwt}`,
+        'apns-topic': bundleId
+      }
 
-	 		session.on('error', sessionErrorHandler);
-	 		session.on('socketError', sessionErrorHandler);
-	 		session.on('goaway', sessionErrorHandler);
+      if (options) {
+        if (options.id) { headers['apns-id'] = options.id }
+        if (options.expiration) { headers['apns-expiration'] = options.expiration }
+        if (options.priority) { headers['apns-priority'] = options.priority }
+        if (options.collapseId) { headers['apns-collapse-id'] = options.collapseId }
+      }
 
-	 		let headers = { 
-	 			':path': `/3/device/${deviceToken}`,
-	 			':method': 'POST',
-	 			'authorization': `bearer ${jwt}`,
-	 			'apns-topic': bundleId
-	 		};
+      const req = session.request(headers)
 
-	 		if (options) {
-	 			if (options.id) { headers['apns-id'] = options.id; }
-	 			if (options.expiration) { headers['apns-expiration'] = options.expiration; }
-	 			if (options.priority) { headers['apns-priority'] = options.priority; }
-	 			if (options.collapseId) { headers['apns-collapse-id'] = options.collapseId; }
-	 		}
+      req.on('aborted', error => {
+        req.close()
+        sessionErrorHandler(error)
+      })
 
-	 		const req = session.request(headers);
+      req.on('response', (headers, flags) => {
+        let data = ''
+        req.on('data', chunk => { data += chunk }).on('end', () => {
+          if (headers[':status'] === 200) {
+            resolve({
+              'status': 200,
+              'apns-id': headers['apns-id']
+            })
+          } else {
+            data = JSON.parse(data)
+            let error
+            let errorText = this.ERRORS[headers[':status']]
+            if (errorText) {
+              error = new Error(errorText)
+              error.reason = data.reason
+              error['apns-id'] = headers['apns-id']
+            } else {
+              error = new Error(`Remote server responded with error code ${headers[':status']}`)
+            }
+            reject(error)
+          }
+          session.destroy()
+        })
+      })
+      const postbody = querystring.stringify(payload)
+      req.end(postbody)
+    })
+  }
 
-	 		req.on('aborted', error => {
-	 			req.close();
-	 			sessionErrorHandler(error);
-	 		});
+  /**
+   * Create a new JWT according to the APNS specifications.
+   *
+   * @param  {String} teamId - The teamId of the organization
+   * @param  {String} keyId - The key id for the application
+   * @param  {String} key - The key to be used for signing
+   *
+   * @return {Promise} A Promise that resolves with the JWT
+   * or rejects if there was an error
+   */
+  createToken (teamId, keyId, key) {
+    return new Promise((resolve, reject) => {
+      if (!teamId) {
+        reject(new Error('Parameter `teamId` is required'))
+        return
+      }
 
-	 		req.on('response', (headers, flags) => {
-	 			let data = '';
-	 			req.on('data', chunk => { data += chunk; }).on('end', () => {
-	 				if (headers[":status"] === 200) {
-	 					resolve({
-	 						"status": 200,
-	 						"apns-id": headers['apns-id']
-	 					});
-	 				} else {
-	 					data = JSON.parse(data);
-	 					let error = undefined;
-	 					let errorText = this.ERRORS[headers[":status"]];
-	 					if (errorText) {
-	 						error = new Error(errorText);
-	 						error.reason = data.reason;
-	 						error["apns-id"] = headers["apns-id"];
-	 					} else {
-	 						error = new Error(`Remote server responded with error code ${headers[':status']}`)
-	 					}
-	 					reject(error);
-	 				}
-	 				session.destroy();
-	 			});
-	 		});
-	 		const postbody = querystring.stringify(payload);
-	 		req.end(postbody);
-	 	});
-	 }
+      if (!keyId) {
+        reject(new Error('Parameter `keyId` is required'))
+        return
+      }
 
-	/**
-	 * Create a new JWT according to the APNS specifications.
-	 * 
-	 * @param  {String} teamId - The teamId of the organization
-	 * @param  {String} keyId - The key id for the application
-	 * @param  {String} key - The key to be used for signing
-	 * 
-	 * @return {Promise} A Promise that resolves with the JWT 
-	 * or rejects if there was an error
-	 */
-	 createToken(teamId, keyId, key) {
-	 	return new Promise((resolve, reject) => {
-	 		if (!teamId) {
-	 			reject( new Error('Parameter `teamId` is required') );
-	 			return;
-	 		}
+      if (!key || key.trim() === '') {
+        reject(new Error('Parameter `key` is required'))
+        return
+      }
 
-	 		if (!keyId) {
-	 			reject( new Error('Parameter `keyId` is required') );
-	 			return;
-	 		}
-
-	 		if (!key || key.trim() === "") {
-	 			reject( new Error('Parameter `key` is required') );
-	 			return;
-	 		}
-
-	 		let signingOptions = {
-	 			issuer: teamId,
-	 			algorithm: 'ES256',
-	 			header: {
-	 				kid: keyId
-	 			}
-	 		};
-	 		jwt.sign({}, key, signingOptions, (err, token) => {
-	 			if (err) {
-	 				reject(err);
-	 			} else {
-	 				try {
-	 					const result = token;
-	 					resolve(result);
-	 				} catch(error) {
-	 					reject(error);
-	 				}
-	 			}
-	 		});
-	 	});
-	 }
-
-	}
+      let signingOptions = {
+        issuer: teamId,
+        algorithm: 'ES256',
+        header: {
+          kid: keyId
+        }
+      }
+      jwt.sign({}, key, signingOptions, (err, token) => {
+        if (err) {
+          reject(err)
+        } else {
+          try {
+            const result = token
+            resolve(result)
+          } catch (error) {
+            reject(error)
+          }
+        }
+      })
+    })
+  }
+}
--- test/module.spec.js
+++ test/module.spec.js
@@ -1,193 +1,193 @@
-const expect = require('chai').expect;
-const ApplePush = require('../index');
-const jwt = require('jsonwebtoken');
-let uuid5 = require("uuid5");
+/* eslint-env mocha */
+'use strict'
 
-const teamid = process.env.teamid || false;
-const keyid = process.env.keyid || false;
-const key = process.env.key || false;
-const devicetoken = process.env.devicetoken || false;
-const bundleid = process.env.bundleid || false;
+const expect = require('chai').expect
+const ApplePush = require('../index')
+const uuid5 = require('uuid5')
+
+const teamid = process.env.teamid || false
+const keyid = process.env.keyid || false
+const key = process.env.key || false
+const devicetoken = process.env.devicetoken || false
+const bundleid = process.env.bundleid || false
 
 if (!teamid || !keyid || !key || !devicetoken) {
-	console.error('Please setup the test environment correctly');
-	process.exit(-1);
+  console.error('Please setup the test environment correctly')
+  process.exit(-1)
 }
 
-describe('Test Suit for apple-push module', function() {
+describe('Test Suit for apple-push module', function () {
+  it('should export a function', function () {
+    expect(ApplePush).is.a('function')
+  })
 
-	it('should export a function', function() {
-		expect(ApplePush).is.a('function');
-	});
+  it('should return new instances of type ApplePush', function () {
+    let apns = new ApplePush()
+    expect(apns instanceof ApplePush).is.eq(true)
+  })
 
-	it('should return new instances of type ApplePush', function() {
-		let apns = new ApplePush();
-		expect(apns instanceof ApplePush).is.eq(true);
-	});
+  describe('Class Structure', function () {
+    let apns = new ApplePush()
 
-	describe('Class Structure', function() {
-		let apns = new ApplePush();
+    it('should contain `url` attribute', function () {
+      expect(apns.url).is.a('string')
+    })
 
-		it('should contain `url` attribute', function() {
-			expect(apns.url).is.a('string');
-		});
+    it('should contain `push` method', function () {
+      expect(apns.push).is.a('function')
+    })
 
-		it('should contain `push` method', function() {
-			expect(apns.push).is.a('function');
-		});
+    it('should contain `createToken` method', function () {
+      expect(apns.createToken).is.a('function')
+    })
+  })
 
-		it('should contain `createToken` method', function() {
-			expect(apns.createToken).is.a('function');
-		});
-	});
+  describe('#createToken()', function () {
+    let apns = new ApplePush()
 
-	describe('#createToken()', function() {
-		let apns = new ApplePush();
-		
-		it('should return a promise', function() {
-			let result = apns.createToken();
-			expect(result).is.a('promise');
-			result.catch(error => error);
-		});
+    it('should return a promise', function () {
+      let result = apns.createToken()
+      expect(result).is.a('promise')
+      result.catch(error => error)
+    })
 
-		context('Missing / Incorrect parameters in function call', function() {
-			it('should check for invalid/missing teamId', function(done) {
-				apns.createToken().then(() => {
-					done(new Error('Check for missing teamId failed'));	
-				}).catch(error => {
-					done();	
-				});
-			});
+    context('Missing / Incorrect parameters in function call', function () {
+      it('should check for invalid/missing teamId', function (done) {
+        apns.createToken().then(() => {
+          done(new Error('Check for missing teamId failed'))
+        }).catch(error => {
+          done()
+        })
+      })
 
-			it('should check for invalid/missing keyId', function(done) {
-				apns.createToken('abcd').then(() => {
-					done(new Error('Check for missing keyId failed'));	
-				}).catch(error => {
-					done();	
-				});
-			});
+      it('should check for invalid/missing keyId', function (done) {
+        apns.createToken('abcd').then(() => {
+          done(new Error('Check for missing keyId failed'))
+        }).catch(error => {
+          done()
+        })
+      })
 
-			it('should check for invalid/missing key', function(done) {
-				apns.createToken('abcd', 'efgh').then(() => {
-					done(new Error('Check for missing key failed'));	
-				}).catch(error => {
-					done();	
-				});
-			});
-		});
+      it('should check for invalid/missing key', function (done) {
+        apns.createToken('abcd', 'efgh').then(() => {
+          done(new Error('Check for missing key failed'))
+        }).catch(error => {
+          done()
+        })
+      })
+    })
 
-		context('Successful function call', function() {
-			it('should return an valid apns jwt token', function(done) {
-				apns.createToken(teamid, keyid, key).then(token => {
-					expect(token).is.a('string');
-					done();
-				}).catch(done);
-			});
-		});
-	});
+    context('Successful function call', function () {
+      it('should return an valid apns jwt token', function (done) {
+        apns.createToken(teamid, keyid, key).then(token => {
+          expect(token).is.a('string')
+          done()
+        }).catch(done)
+      })
+    })
+  })
 
+  describe('#push()', function () {
+    let apns
+    let token
 
-	describe('#push()', function() {
-		let apns = undefined;
-		let token = undefined;
-		
-		before(async function() {
-			apns = new ApplePush();
-			token = await apns.createToken(teamid, keyid, key);
-		});
+    before(async function () {
+      apns = new ApplePush()
+      token = await apns.createToken(teamid, keyid, key)
+    })
 
-		it('should return a promise', function() {
-			let result = apns.push();
-			expect(result).is.a('promise');
-			result.catch(error => error);
-		});
+    it('should return a promise', function () {
+      let result = apns.push()
+      expect(result).is.a('promise')
+      result.catch(error => error)
+    })
 
-		context('Missing / Incorrect parameters in function call', function() {
-			it('should check for invalid/missing payload', function(done) {
-				apns.push().then(() => {
-					done(new Error('Check for missing payload failed'));	
-				}).catch(error => {
-					done();	
-				});
-			});
-			
-			it('should check for invalid/missing jwt', function(done) {
-				apns.push('abcd').then(() => {
-					done(new Error('Check for missing jwt failed'));	
-				}).catch(error => {
-					done();	
-				});
-			});
+    context('Missing / Incorrect parameters in function call', function () {
+      it('should check for invalid/missing payload', function (done) {
+        apns.push().then(() => {
+          done(new Error('Check for missing payload failed'))
+        }).catch(error => {
+          done()
+        })
+      })
 
-			it('should check for invalid/missing deviceToken', function(done) {
-				apns.push('abcd', 'qwe123').then(() => {
-					done(new Error('Check for missing deviceToken failed'));	
-				}).catch(error => {
-					done();	
-				});
-			});
+      it('should check for invalid/missing jwt', function (done) {
+        apns.push('abcd').then(() => {
+          done(new Error('Check for missing jwt failed'))
+        }).catch(error => {
+          done()
+        })
+      })
 
-			it('should check for invalid/missing bundleId', function(done) {
-				apns.push('abcd', 'qwe123', 'naksdk').then(() => {
-					done(new Error('Check for missing bundleId failed'));	
-				}).catch(error => {
-					done();	
-				});
-			});
-		});
+      it('should check for invalid/missing deviceToken', function (done) {
+        apns.push('abcd', 'qwe123').then(() => {
+          done(new Error('Check for missing deviceToken failed'))
+        }).catch(error => {
+          done()
+        })
+      })
 
-		context('Graceful handling of request errors', function() {
-			it('should handle 403 errors', function(done) {
-				this.timeout(10000);
-				const payload = {
-					"aps" : {
-						"badge" : 9,
-						"sound" : "bingbong.aiff"
-					},
-					"messageID" : "ABCDEFGHIJ"
-				}
-				apns.push(payload, 'token', devicetoken, {topic: "com.patch.NotificationReceiverPatch"}).then(res => {
-					done(new Error('Failed to handle the 403 error'));
-				}).catch(error => {
-					done();
-				});
-			});
-		});
+      it('should check for invalid/missing bundleId', function (done) {
+        apns.push('abcd', 'qwe123', 'naksdk').then(() => {
+          done(new Error('Check for missing bundleId failed'))
+        }).catch(error => {
+          done()
+        })
+      })
+    })
 
-		context('Successful Requests', function() {
-			it('should resolve successfully', function(done) {
-				this.timeout(10000);
-				const payload = {
-					"aps" : {
-						"badge" : 9,
-						"sound" : "bingbong.aiff"
-					},
-					"messageID" : "ABCDEFGHIJ"
-				}
-				apns.push(payload, token, devicetoken, bundleid).then(res => {
-					done();
-				}).catch(done);
-			});
+    context('Graceful handling of request errors', function () {
+      it('should handle 403 errors', function (done) {
+        this.timeout(10000)
+        const payload = {
+          'aps': {
+            'badge': 9,
+            'sound': 'bingbong.aiff'
+          },
+          'messageID': 'ABCDEFGHIJ'
+        }
+        apns.push(payload, 'token', devicetoken, { topic: 'com.patch.NotificationReceiverPatch' }).then(res => {
+          done(new Error('Failed to handle the 403 error'))
+        }).catch(error => {
+          done()
+        })
+      })
+    })
 
-			it('should use the given options', function(done) {
-				this.timeout(10000);
-				const payload = {
-					"aps" : {
-						"badge" : 9,
-						"sound" : "bingbong.aiff"
-					},
-					"messageID" : "ABCDEFGHIJ"
-				};
-				const options = {
-					id: uuid5(bundleid + Date.now()),
-					expiration: 30,
-					priority: 5,
-					collapseId: `${Math.ceil(Math.random() * 10000)}`
-				};
-				apns.push(payload, token, devicetoken, bundleid, options).then(res => {
-					done();
-				}).catch(done);
-			});
-		});
-	});
-});
+    context('Successful Requests', function () {
+      it('should resolve successfully', function (done) {
+        this.timeout(10000)
+        const payload = {
+          'aps': {
+            'badge': 9,
+            'sound': 'bingbong.aiff'
+          },
+          'messageID': 'ABCDEFGHIJ'
+        }
+        apns.push(payload, token, devicetoken, bundleid).then(res => {
+          done()
+        }).catch(done)
+      })
+
+      it('should use the given options', function (done) {
+        this.timeout(10000)
+        const payload = {
+          'aps': {
+            'badge': 9,
+            'sound': 'bingbong.aiff'
+          },
+          'messageID': 'ABCDEFGHIJ'
+        }
+        const options = {
+          id: uuid5(bundleid + Date.now()),
+          expiration: 30,
+          priority: 5,
+          collapseId: `${Math.ceil(Math.random() * 10000)}`
+        }
+        apns.push(payload, token, devicetoken, bundleid, options).then(res => {
+          done()
+        }).catch(done)
+      })
+    })
+  })
+})
